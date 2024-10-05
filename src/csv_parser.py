d8fed5119b5eeb9f72d28def2a8e5d70a628da17
LockedInTheSkage <LockedInTheSkage@users.noreply.github.com>
2024-10-05 17:26:26 +0200

    Add parsers

--- src/csv_parser.py
+++ src/csv_parser.py
@@ -22,19 +22,53 @@ def parse_time(raw_time):
         return None
 
 def retrieve_ais(path):
-    df_ais = pd.read_csv('../resources/ais_train.csv', sep='|')
-    df_ais.head()
+    df_ais = pd.read_csv(path, sep='|')
     df_ais['time'] = pd.to_datetime(df_ais['time'])
     df_ais['etaParsed'] = df_ais['etaRaw'].apply(parse_time)
-    df_ais.drop(['etaRaw'], axis = 1)
-
-    df_ais.isna().sum() #Estimated arrival time (1615 entries) and portId (1615 entries)
-
-    df_ais.loc[df_ais.duplicated()] #No duplicated rows
-
+    df_ais=df_ais.drop(['etaRaw'], axis = 1)
     return df_ais
 
+def retrieve_ports(path):
+    df_ports = pd.read_csv(path, sep='|')
+    df_ports['portLongitude'] = df_ports['longitude']
+    df_ports['portLatitude'] = df_ports['latitude']
+    df_ports=df_ports.drop(['name', 'portLocation', 'countryName', 'longitude', 'latitude'], axis = 1)
+    return df_ports
+
+def retrieve_schedules(path):
+    df_schedules = pd.read_csv(path, sep='|')
+    df_schedules['sailingDate'] = pd.to_datetime(df_schedules['sailingDate'])
+    df_schedules['arrivalDate'] = pd.to_datetime(df_schedules['arrivalDate'])
+
+    df_schedules=df_schedules.drop(['shippingLineName', 'portName', 'portLongitude', 'portLatitude'], axis = 1)
+    print(df_schedules.head())
+    return df_schedules
+
+def retrieve_vessels(path):
+    df_vessels = pd.read_csv(path, sep='|')
+    return df_vessels
+
+def retrieve_tests(path):
+    df_tests = pd.read_csv(path, sep='|')
+    df_tests['time'] = pd.to_datetime(df_tests['time'])
+    return df_tests
+
 def retrieve_all_data():
+    folderpath = 'resources/'
+    df_ais=retrieve_ais(folderpath+'ais_train.csv')
+    df_ports=retrieve_ports(folderpath+'ports.csv')
+    df_schedules=retrieve_schedules(folderpath+'schedules_to_may_2024.csv')
+    df_vessels=retrieve_vessels(folderpath+'vessels.csv')
+    df_tests=retrieve_tests(folderpath+'ais_test.csv')
+
     
-    
+    result = pd.merge(df_ais, df_ports, on='portId')
+    result = pd.merge(result, df_vessels, on='vessel_id')
+    result = pd.merge(result, df_schedules, on='portId')
+    result = result.drop(['portId'], axis = 1)
+    print(result.head())
+
+
+if __name__ == '__main__':
+    retrieve_all_data()
     
