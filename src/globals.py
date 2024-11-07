45da3e8e3b355cdbef3ed0f07b61aff9b4bd12e3
FuschiaPrince <sdpalenc@stud.ntnu.no>
2024-11-07 13:24:38 +0100

    shit

--- src/globals.py
+++ src/globals.py
@@ -1,8 +1,8 @@
-RESOURCE_FOLDER="../../resources"
+RESOURCE_FOLDER="../../Project materials(1)"
 MARKOV_SIZE=2
 STEPSIZES = [1] #3, 6, 18, 36, 72, 144, 216, 288, 360]
 OUTPUT_WINDOW = 1
 INPUT_WINDOW = 4
-OUTPUT_FORECAST = ["latitude", "longitude"]
-ONE_HOT_COLUMNS = ["navstat_cat", "rot_cat"]
+OUTPUT_FORECAST = ["latitude", "longitude","sog", "cog"]
+ONE_HOT_COLUMNS = ["navstat_cat"] #rot_cat
 DELETEABLE_COLUMNS = ["vesselId", "portId", "etaParsed", "UN_LOCODE", "ISO", "navstat", "rot"]
